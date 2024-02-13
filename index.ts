f9449d1e70906cf2a24c3dce478d3dfe3ebacf96
Anuj Kumar <kumaranujam1009@gmail.com>
2024-02-13 13:40:15 +0530

    mongodb connection

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require('express')
+const app = express()
+const { databaseConnection } = require('./config/database')
+
+require('dotenv').config()
+databaseConnection(process.env.URL,()=>{
+    console.log("mongodb connected....")
+})
+
+app.listen(4000,()=>{
+    console.log("server listening on 4000")
+})
