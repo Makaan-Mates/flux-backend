6f1456514b688620e6c220e0a748cb56966575cb
shadevkumar <shadevsrk261@gmail.com>
2024-02-27 00:54:01 +0530

    bookmarknotes and getusernotes routes added

--- src/api/controllers/getUserNotes.ts
+++ src/api/controllers/getUserNotes.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from "express";
 import Notes from "../models/notes.model.js";
 import User from "../models/user.model.js";
-export const getFluxDetail = async (req: Request, res: Response) => {
-  const videoId = req.query.videoId;
+export const getUserNotes = async (req: Request, res: Response) => {
   const email = req.query.email;
   const author = await User.findOne({ email: email });
   try {
-    const fluxDetail = await Notes.findOne({
-      videoId: videoId,
+    const userNotes = await Notes.find({
       authorId: author?._id,
     });
     res.status(200).json({
-      message: fluxDetail,
+      message: userNotes,
     });
   } catch (err) {
     console.log(err);
--- src/api/routes/flux.route.ts
+++ src/api/routes/flux.route.ts
@@ -1,9 +1,14 @@
 import express from "express";
-
 const router = express.Router();
 import generateSummary from "../controllers/generateSummary.js";
 import { getFluxDetail } from "../controllers/getFluxDetail.js";
+import {bookmarkFlux} from "../controllers/bookmarkFlux.js"
+import {getUserNotes} from "../controllers/getUserNotes.js"
+
 router.post("/createflux", generateSummary);
 router.get("/getfluxdetail", getFluxDetail);
+router.post("/bookmarkflux",bookmarkFlux)
+router.get("/getUserNotes",getUserNotes)
+
 
 export default router;
