64d49d85d84283c6efa18f0fb21cb9fb69a2ac39
Sourabh <rathoursourabh5@gmail.com>
2024-03-15 04:03:34 +0530

    share routes added

---
+++ src/api/controllers/submitNotionPageId.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import User from "../models/user.model.js";
+
+export const submitNotionPageId = async (req: Request, res: Response) => {
+  const pageId = req.body.page_id;
+  const email = req.body.email;
+
+  console.log("email: " + email);
+
+  const user = await User.findOne({ email: email });
+  console.log("user: " + user);
+  if (!user) return res.status(404).json({ error: "User not found." });
+
+  //@ts-ignore
+  user.notionPageId = pageId;
+  await user.save();
+
+  res.status(200).json({
+    message: "Successfully updated Notion Page ID.",
+  });
+};
--- src/api/routes/flux.route.ts
+++ src/api/routes/flux.route.ts
@@ -2,7 +2,10 @@ import express from "express";
 const router = express.Router();
 import generateSummary from "../controllers/generateSummary.js";
 import { getFluxDetail } from "../controllers/getFluxDetail.js";
-import { bookmarkFlux,deleteBookMarkedNote } from "../controllers/bookmarkFlux.js";
+import {
+  bookmarkFlux,
+  deleteBookMarkedNote,
+} from "../controllers/bookmarkFlux.js";
 import { getUserNotes } from "../controllers/getUserNotes.js";
 import { getBookmarkStatus } from "../controllers/getBookmarkStatus.js";
 import { getAccessToken } from "../controllers/getAccessToken.js";
@@ -10,6 +13,7 @@ import { createNotionPage } from "../controllers/createNotionPage.js";
 import { appendContent } from "../controllers/appendContent.js";
 import { fetchNotionPages } from "../controllers/fetchNotionPages.js";
 import { rateLimitMiddleware } from "../middlewares/rateLimitMiddleware.js";
+import { getSharedNotes } from "../controllers/getSharedNotes.js";
 
 router.post("/createflux", rateLimitMiddleware, generateSummary);
 router.get("/getfluxdetail", getFluxDetail);
@@ -21,4 +25,5 @@ router.post("/accesstoken", getAccessToken);
 router.post("/create/notionpage", createNotionPage);
 router.post("/appendcontent", appendContent);
 router.post("/fetchpages", fetchNotionPages);
+router.get("/notes/shared/:noteId", getSharedNotes);
 export default router;
